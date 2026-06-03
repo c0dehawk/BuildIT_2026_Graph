8e77eef44261a2037b830e2648adf20177644692
Jason Thomas <jason.t@outlook.com>
2026-06-03 15:12:40 -0500

    Add Console Application

---
+++ BuildIT_2026.Graph.Console/Program.cs
@@ -0,0 +1,59 @@
+﻿using BuildIT2026_Graph.Core.Models;
+using MockingBird;
+using BuildIT2026_Graph.Core.Services;
+using Newtonsoft.Json;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("=== BuildIT Graph Demo ===");
+    Console.WriteLine("");
+    Console.WriteLine("1. Seed Customer Nodes");
+    Console.WriteLine("10. Exit");
+    Console.Write("Select an option: ");
+
+    string choice = Console.ReadLine() ?? string.Empty;
+
+    switch (choice)
+    {
+        case "1":
+            SeedCustomerNodes().Wait();
+            break;
+        case "10":
+            Console.WriteLine("Exiting application. Goodbye!");
+            return;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+
+    static async Task SeedCustomerNodes()
+    {
+        Console.Write("How many records do you want to generate? ");
+        var input = Console.ReadLine();
+
+        var numberOfRecords = int.TryParse(input, out int n) ? n : 10;
+
+        MBObject obj = new MBObject();
+        obj.AddProperty(new MBStringProperty("Id").SetFormat(MBStringFormat.Guid));
+        obj.AddProperty(new MBStringProperty("FirstName").SetFormat(MBStringFormat.FirstName));
+        obj.AddProperty(new MBStringProperty("LastName").SetFormat(MBStringFormat.LastName));
+        obj.AddProperty(new MBDateTimeProperty("DateOfBirth"));
+        obj.AddProperty(new MBStringProperty("Email").SetFormat(MBStringFormat.Email));
+
+        var sampleRecords = obj.Mock(numberOfRecords); 
+
+
+        //// Cast to Person
+        //List<Object> people = JsonConvert.DeserializeObject<List<Object>>(JsonConvert.SerializeObject(sampleRecords)) ?? new List<Object>();
+
+        //Console.Write(JsonConvert.SerializeObject(people, Newtonsoft.Json.Formatting.Indented));
+
+    } 
+
+    Console.WriteLine("\nPress Enter to return to the menu...");
+    Console.ReadLine();
+}
